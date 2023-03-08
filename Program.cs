c6479588fffacf477c795a30ca72cf57766a174e
Rickanjo <ricardo.cassettari07@gmail.com>
2023-03-08 13:17:35 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿Console.WriteLine("Musica Favorita DELA");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("This Town - niallhoran");
+Console.ResetColor();
+Console.WriteLine("");
+
+Console.WriteLine("Waking up to kiss you and nobody's there");
+Console.WriteLine("The smell of your perfume still stuck in the air");
+Console.WriteLine("It's hard");
+Console.WriteLine("Yesterday I thought I saw your shadow running round");
+Console.WriteLine("It's funny how things never change in this old town");
+Console.WriteLine("So far from the stars");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.DarkYellow
+Console.WriteLine("And I want to tell you everything");
+Console.WriteLine("The words I never got to say the first time around");
+Console.WriteLine("And I remember everything");
+Console.WriteLine("From when we were the children playing in this fairground");
+Console.WriteLine("Wish I was there with you now");
+Console.ResetColor();
+Console.WriteLine("");
+
+Console.WriteLine("If the whole world was watching I'd still dance with you");
+Console.WriteLine("Drive highways and byways to be there with you");
+Console.WriteLine("Over and over the only truth");
+Console.WriteLine("Everything comes back to you");
+Console.WriteLine("");
+
+Console.WriteLine("I saw that you moved on with someone new");
+Console.WriteLine("In the pub that we met he's got his arms around you");
+Console.WriteLine("It's so hard");
+Console.WriteLine("So hard");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.DarkYellow
+Console.WriteLine("And I want to tell you everything");
+Console.WriteLine("The words I never got to say the first time around");
+Console.WriteLine("And I remember everything");
+Console.WriteLine("From when we were the children playing in this fairground");
+Console.WriteLine("Wish I was there with you now");
+Console.ResetColor();
+Console.WriteLine("");
+
+Console.WriteLine("Because if the whole world was watching I'd still dance with you");
+Console.WriteLine("Drive highways and byways to be there with you");
+Console.WriteLine("Over and over the only truth");
+Console.WriteLine("Everything comes back to you");
+Console.WriteLine("You still make me nervous when you walk in the room");
+Console.WriteLine("Them butterflies they come alive when I'm next to you");
+Console.WriteLine("Over and over the only truth");
+Console.WriteLine("Everything comes back to you");
+Console.WriteLine("");
+
+Console.WriteLine("And I know that it's wrong");
+Console.WriteLine("That I can't move on");
+Console.WriteLine("But there's something about you");
+Console.WriteLine("");
+
+Console.WriteLine("If the whole world was watching I'd still dance with you");
+Console.WriteLine("Drive highways and byways to be there with you");
+Console.WriteLine("Over and over the only truth");
+Console.WriteLine("Everything comes back to you");
+Console.WriteLine("You still make me nervous when you walk in the room");
+Console.WriteLine("Them butterflies they come alive when I'm next to you");
+Console.WriteLine("Over and over the only truth");
+Console.WriteLine("Everything comes back to you");;
+Console.WriteLine("Everything comes back to you");
+
